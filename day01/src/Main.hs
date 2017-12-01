30eb91500b9b1ee584ab597276653f7cf1d8e790
Edward Wastell <ed@wastell.co.uk>
2017-12-01 14:33:35 +0000

    Did day one

--- day01/src/Main.hs
+++ day01/src/Main.hs
@@ -1,4 +1,26 @@
 module Main where
 
+import           Data.Char
+
+readInput :: IO [Int]
+readInput = map (read . pure) . filter isNumber <$> readFile "input.txt"
+
+partA :: (Num a, Eq a) => [a] -> a
+partA [] = 0
+partA (x:xs) = sum $ map fst $ filter (uncurry (==)) $ zip (x : xs) (xs ++ [x])
+
+partB :: (Num a, Eq a) => [a] -> a
+partB [] = 0
+partB xs =
+  let l = length xs
+      helper n = xs !! ((n + l `div` 2) `mod` l)
+  in sum $ zipWith (\n x -> if helper n == x then x else 0) [0..] xs
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  i <- readInput
+  putStrLn "Day One"
+  putStr $ "\tPart A: "
+  print $ partA i
+  putStr $ "\tPart B: "
+  print $ partB i
