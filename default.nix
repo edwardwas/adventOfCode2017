ea935fa899169d699d76538ceae4c3566672cf66
Ed Wastell <ed@wastell.co.uk>
2017-12-03 11:40:16 +0000

    Started day03

--- default.nix
+++ default.nix
@@ -3,6 +3,7 @@ let
   hp = pkgs.haskellPackages;
 in
   {
-    day01 = hp.callPackage ./nix/day01.nix { };
-    day02 = hp.callPackage ./nix/day02.nix { };
+    day01 = hp.callCabal2nix "day01" ./day01 { };
+    day02 = hp.callCabal2nix "day02" ./day02 { };
+    day03 = hp.callCabal2nix "day03" ./day03 { };
   }
