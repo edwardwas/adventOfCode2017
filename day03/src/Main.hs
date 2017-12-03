0409d2df6b6576fddbfc2d88610acb5c9abcb161
Ed Wastell <ed@wastell.co.uk>
2017-12-03 12:52:02 +0000

    Day 03 done

--- day03/src/Main.hs
+++ day03/src/Main.hs
@@ -1,4 +1,34 @@
 module Main where
 
+spiralHelper ::
+     (Int -> (Int, Int) -> a) -> Int -> Int -> Int -> Int -> Int -> [a]
+spiralHelper func n x y dx dy =
+  let (dx', dy') =
+        if x == y || (x < 0 && x == -y) || (x > 0 && x == 1 - y)
+          then (-dy, dx)
+          else (dx, dy)
+  in func n (x, y) : spiralHelper func (n + 1) (x + dx') (y + dy') dx' dy'
+
+spiral :: (Int -> (Int, Int) -> a) -> [a]
+spiral func = spiralHelper func 1 0 0 0 (-1)
+
+partA :: Int -> Int
+partA i = spiral (\_ (x, y) -> abs x + abs y) !! i
+
+partB :: Int -> Int
+partB i =
+  let helper 1 p = (1, p)
+      helper n (x, y) =
+        let near (a, b) = (a - x) * (a - x) + (b - y) * (b - y) < 4
+        in (sum $ map fst $ filter (near . snd) $ take (n - 1) res, (x, y))
+      res = spiral helper
+  in head $ filter (> i) $ map fst res
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  let i = 265149
+  putStrLn "Day 03"
+  putStr "\tPart A: "
+  print $ partA i
+  putStr "\tPart B: "
+  print $ partB i
