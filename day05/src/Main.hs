0b30aa9f1979cba6badf7437c4a6981a14c1b6a6
Ed Wastell <ed@wastell.co.uk>
2017-12-05 11:43:41 +0000

    Did day 5

--- day05/src/Main.hs
+++ day05/src/Main.hs
@@ -1,56 +1,39 @@
-{-# LANGUAGE DeriveTraversable     #-}
-{-# LANGUAGE FlexibleContexts      #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE TemplateHaskell       #-}
-{-# LANGUAGE TypeFamilies          #-}
+{-# LANGUAGE TemplateHaskell #-}
 
 module Main where
 
-import           Control.Comonad
-import           Control.Comonad.Store
-import           Control.Lens
-import           Data.FileEmbed
-import qualified Data.Vector           as V
-import           GHC.Exts
+import           Control.Monad       (zipWithM_)
+import           Data.FileEmbed      (embedStringFile)
+import qualified Data.Vector.Mutable as MV
 
-instructions :: Focused Int
-instructions = fromList $ map read $ lines $(embedStringFile "input.txt")
+jumpHelper :: (Int -> Int) -> Int -> Int -> MV.IOVector Int -> IO Int
+jumpHelper changeFunction count pos v = do
+  here <- MV.read v pos
+  let newPos = pos + here
+  MV.modify v changeFunction pos
+  if newPos >= MV.length v
+    then return (count + 1)
+    else jumpHelper changeFunction (count + 1) newPos v
 
-data Focused a =
-  Focused Int
-          (V.Vector a)
-  deriving (Eq, Show, Functor, Foldable, Traversable)
+escapeLength :: (Int -> Int) -> [Int] -> IO Int
+escapeLength changeFunc ns = do
+  v <- MV.new $ length ns
+  zipWithM_ (MV.write v) [0 ..] ns
+  jumpHelper changeFunc 0 0 v
 
-instance IsList (Focused a) where
-  type Item (Focused a) = a
-  toList (Focused _ v) = toList v
-  fromList xs = Focused 0 $ V.fromList xs
+instructions :: [Int]
+instructions = map read $ lines $(embedStringFile "input.txt")
 
-currentFocused :: Lens' (Focused a) a
-currentFocused =
-  lens
-    (\(Focused n v) -> v V.! n)
-    (\(Focused n v) a -> Focused n $ v & ix n .~ a)
-
-instance Comonad Focused where
-  extract (Focused n v) = v V.! n
-  duplicate (Focused n v) =
-    Focused n $ V.fromList $ map (\n' -> Focused n' v) [0 .. V.length v]
-
-instance ComonadStore Int Focused where
-  pos (Focused n _) = n
-  peek n (Focused _ v) = v V.! n
-
-handleJump :: (Int -> Int) -> Int -> Focused Int -> Int
-handleJump changeFunc count v
-  | length v <= pos v + extract v = count + 1
-  | otherwise =
-    handleJump changeFunc (count + 1) $ seeks (+ extract v) $ v & currentFocused %~ changeFunc
-
-main :: IO ()
 main = do
   putStrLn "Day 05"
   putStr "\tPart A: "
-  print $ handleJump (+ 1) 0 instructions
+  escapeLength (+ 1) instructions >>= print
   putStr "\tPart B: "
-  print $ handleJump (\x -> if x >= 3 then x - 1 else x + 1) 0 instructions
+  escapeLength
+    (\x ->
+       if x >= 3
+         then x - 1
+         else x + 1)
+    instructions >>=
+    print
+
